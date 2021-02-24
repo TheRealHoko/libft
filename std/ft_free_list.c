0146e0e5016292a78148110588221b3ecd8347cb
TheRealHoko <julien.zeybel@live.fr>
2021-02-24 19:52:37 +0100

    parse with sp tab comma

--- std/ft_free_list.c
+++ std/ft_free_list.c
@@ -6,13 +6,13 @@
 /*   By: jzeybel <jzeybel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 18:00:37 by jzeybel           #+#    #+#             */
-/*   Updated: 2021/02/24 18:02:52 by jzeybel          ###   ########.fr       */
+/*   Updated: 2021/02/24 19:51:09 by jzeybel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/libft.h" 
 
-void	*free_list(void	**list)
+void	**ft_free_list(void	**list)
 {
 	int	i;
 	void	**k;
